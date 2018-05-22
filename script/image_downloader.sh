cc965b4c18b55bdb32613762c5415d88447015d2
Toru3 <ryuutet@gmail.com>
2018-05-23 00:25:13 +0900

    image_downloaderを改良

--- script/image_downloader.sh
+++ script/image_downloader.sh
@@ -1,54 +1,70 @@
 #!/bin/sh -eu
-dl_prog='wget -nv'
+umask 0022
+export LC_ALL='C' PATH="$(command -p getconf PATH):$PATH"
+
+if command -v wget; then
+    dl(){
+        if [ ! -e $2 ]; then
+            wget -nv $1/$2 -O $2
+            sleep 1
+        fi
+    }
+elif command -v curl; then
+    dl(){
+        if [ ! -e $2 ]; then
+            curl $1/$2 -o $2
+            sleep 1
+        fi
+    }
+else
+    echo "wget or curl is required."
+    exit 1
+fi
+
 root_url='http://mucho.girly.jp/bona'
 mkcd(){
     mkdir -p $1
     cd $1
 }
-cd ..
+
 mkcd image
-root_dir=`pwd`
 
 i=focus
-cd $root_dir
 mkcd $i
-url=$root_url/$i/
+url=$root_url/$i
 for j in thin trpt bold; do
     for k in b g r y; do
-        $dl_prog $url/${i}_${j}_${k}.png
-        sleep 1
+        dl $url ${i}_${j}_${k}.png
     done
 done
-$dl_prog $url/${i}_bold_o.png
+dl $url ${i}_bold_o.png
+cd ..
 
 i=koma
 for j in kinki kinki_torafu ryoko ryoko_torafu dirty; do
-    cd $root_dir
     mkcd ${i}/${i}_${j}
     url=$root_url/$i/${i}_${j}
     for k in G S; do
         for l in fu to kyo kei nkei gin ngin kin ou hi ryu kaku uma; do
-            $dl_prog $url/$k$l.png
-            sleep 1
+            dl $url $k$l.png
         done
     done
+    cd ../..
 done
 
 i=ban
-cd $root_dir
 mkcd $i
-url=$root_url/$i/
+url=$root_url/$i
 for j in kaya_a kaya_b kaya_c kaya_d gohan oritatami dirty paper stripe muji; do
-    $dl_prog $url/${i}_${j}.png
-    sleep 1
+    dl $url ${i}_${j}.png
 done
+cd ..
 
 i=masu
-cd $root_dir
 mkcd $i
-url=$root_url/$i/
+url=$root_url/$i
 for j in dot nodot dot_xy nodot_xy handwriting; do
-    $dl_prog $url/${i}_${j}.png
-    sleep 1
+    dl $url ${i}_${j}.png
 done
+cd ..
 
